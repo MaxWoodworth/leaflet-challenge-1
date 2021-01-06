1bc769e3ec6702e93ab8f82610af9cfda4bcd9f3
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2021-01-06 00:11:16 -0600

    - Adds function for color using depth

--- logic.js
+++ logic.js
@@ -3,23 +3,27 @@
 var myMap = L.map("map", {
     center: [37.0902, -95.7129],
     zoom: 5.4
-  });
+});
 
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
 }).addTo(myMap);
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-function markerSize(magnitude){
+function markerSize(magnitude) {
     return magnitude;
 }
-d3.json(url, function(response) {
+function quakeColor(depth) {
+    return depth;
+}
+
+d3.json(url, function (response) {
 
     var latitude = [];
     var longitude = [];
@@ -27,15 +31,14 @@ d3.json(url, function(response) {
     var magnitude = [];
 
     console.log(response);
-  
+
     for (var i = 0; i < response.length; i++) {
-      var location = response[i].geometry;
-    //   var depth = response[i]
-      if (location) {
-        L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
-      }
+        var location = response[i].geometry;
+        //   var depth = response[i]
+        if (location) {
+            L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
+        }
     }
-  
-  });
-  
-  
+
+});
+
