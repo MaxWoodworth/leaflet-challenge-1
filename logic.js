2bf38f7d32697946eadaf32e56bb5b9704e17d47
walaha2002 <68665730+walaha2002@users.noreply.github.com>
2021-01-06 00:10:07 -0600

    - Adds function for marker size

--- logic.js
+++ logic.js
@@ -16,12 +16,20 @@ var myMap = L.map("map", {
 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+function markerSize(magnitude){
+    return magnitude;
+}
 d3.json(url, function(response) {
+
+    var latitude = [];
+    var longitude = [];
+    var depth = [];
+    var magnitude = [];
 
     console.log(response);
   
     for (var i = 0; i < response.length; i++) {
-      var location = response[i].location;
+      var location = response[i].geometry;
     //   var depth = response[i]
       if (location) {
         L.marker([location.coordinates[1], location.coordinates[0]]).addTo(myMap);
